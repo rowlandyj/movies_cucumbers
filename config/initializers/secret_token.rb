0f21ee5b15f3374765a681725eb6ec7995db4436
Apprentice <apprentice@devbootcamp.com>
2013-07-25 19:13:23 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviesCucumbers::Application.config.secret_token = '5c5c2b50a65e4670bc292909269a02b374d4dcfa1954d2af57508f343cd9d496843617232ece1ac9edcef81501438c4abbfcc1a27bca180599f30a93dc54c883'
