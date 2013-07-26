064f629bea576987fd99e9e4d4d39378d15e655b
Brian Yee <byee322@gmail.com>
2013-07-26 16:46:35 -0700

    added routes!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,41 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def person_correlation(user_1_ratings, user_2_ratings)
+  shared_items = user_1_ratings.keys.reject{|k| user_2_ratings[k].nil? }
+  number_of_shared_items = shared_items.length.to_f
+
+  #if there is nothing in commin return 0 
+  return 0 if number_of_shared_items == 0
+   
+  averge_rating_of_user_1 = shared_items.inject(0) do |agg, k|
+    agg + user_1_ratings[k]
+  end
+
+  averge_rating_of_user_1 = averge_rating_of_user_1 / number_of_shared_items
+
+  averge_rating_of_user_2 = shared_items.inject(0) do |agg, k|
+    agg + user_2_ratings[k]
+  end
+
+  averge_rating_of_user_2 = averge_rating_of_user_2 / number_of_shared_items
+
+  numerator = shared_items.inject(0) do |agg, k|
+    user_1 = user_1_ratings[k] - averge_rating_of_user_1
+    user_2 = user_2_ratings[k] - averge_rating_of_user_2
+    agg + (user_1 * user_2)
+  end
+
+  square_1 = shared_items.inject(0) do |agg, k| 
+    agg + ((user_1_ratings[k] + averge_rating_of_user_1)**2)
+  end 
+  square_2 = shared_items.inject(0) do |agg, k| 
+    agg + ((user_2_ratings[k] + averge_rating_of_user_2)**2)
+  end 
+
+  denominator = Math.sqrt(square_1 * square_2)
+
+  return numerator.to_f / denominator.to_f
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 MoviesCucumbers::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
-  end
-  root :to => "home#index"
-  devise_for :users
-  resources :users
+	authenticated :user do
+		root :to => 'home#index'
+	end
+	devise_for :users
+	resources :users do 
+		resources :ratings, shallow: true
+		member do
+			get 'recommendations'
+			get 'watchlist'
+			post 'watchlist'
+			put 'watchlist'
+			get 'favorites'
+			post 'favorites'
+			put 'favorites'
+		end
+	end
+
 end
