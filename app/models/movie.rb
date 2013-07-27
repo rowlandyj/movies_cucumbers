3ca4c6846f31c6dcf5d4a3813e2b84cf42d84a04
Apprentice <apprentice@dbc19.local>
2013-07-27 16:51:37 -0700

    successfully rated a movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -152,6 +152,9 @@ class Movie < ActiveRecord::Base
 
   def self.unit_cluster(rated_movie, rating)
 
+    debugger 
+    ''
+
     directors = rated_movie.directors
     actors = rated_movie.actors
     genres = rated_movie.genres
