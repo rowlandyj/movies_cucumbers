9f41cb327dc97e3a4565ea01932cda3377fc0353
Apprentice <apprentice@dbc19.local>
2013-07-27 14:11:54 -0700

    added create controller stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,8 @@ class ApplicationController < ActionController::Base
   #     redirect_to new_user_session_path
   #   end
   # end
+
+  
     
 
 end
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,8 +3,17 @@ class RatingsController < ApplicationController
     @movies = Movie.find(:all).sample(50)
   end
   def create
+    rating = Rating.new(params[:rating])
+    if rating.save
+      current_user.ratings << rating
+      update_recommendations
+    else
+      #show error message
+      redirect_to new_user_rating_path
+    end
   end
   def new
+    rating = Rating.new
   end
   def edit
   end
