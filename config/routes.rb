b4b88583493b89e1d313276ba1c7d7d4ca96260e
Apprentice <apprentice@dbc19.local>
2013-07-31 14:31:26 -0700

    axed out lots of unnecessary devise stuff woo

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 MoviesCucumbers::Application.routes.draw do
 	root :to => 'home#index'
 	devise_for :users
-	# authenticated :user do
-	# 	root :to => 'home#index'
-	# end
 
 	resources :recommendations, only: [:index, :create, :new] 
 	resources :ratings, only: [:index, :new, :create, :edit, :update]
 
 	get '/search' => 'ratings#search', as: 'search'
 
-	resources :user, only: [:index, :show]
-
-
 	get '/users/ratings' => 'users#ratings', as: 'user_ratings'
 
-	# resources :favorites, only: [:index, :new, :create, :edit, :update]
-	# resources :watch_list, only: [:index, :new, :create, :edit, :update]
-
-	
-	
 end
